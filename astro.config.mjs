4c116dabec4f455806338acdf9335a837f17f3ad
languint <longuint@proton.me>
2025-03-05 13:23:57 -0600

    Update docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://languint.github.io",
+  base: "/manim-docs",
+  integrations: [
+    starlight({
+      title: "@rbxts/manim",
+      logo: { src: "./src/assets/logo.svg", replacesTitle: true },
+      customCss: ["./src/style.css"],
+      social: {
+        github: "https://github.com/languint/manim",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Starting Guide", slug: "guides/starting" },
+            { label: "Initialization", slug: "guides/initialization" },
+          ],
+        },
+        {
+          label: "Reference",
+          items: [
+            "reference/registry",
+            {
+              label: "Objects",
+              items: [
+                "reference/objects/mobject",
+                "reference/objects/scene",
+                "reference/objects/scenewithcamera",
+              ],
+            },
+            {
+              label: "Decorators",
+              items: [
+                "reference/decorators/ticker",
+                "reference/decorators/disabled",
+                "reference/decorators/scene",
+              ],
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
